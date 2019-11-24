125835005947578b7d9a364b7ac5b3aceb29673c
malhar <malhars@sas.upenn.edu>
2019-11-24 12:33:51 -0500

    connection to db is working

--- routes.js
+++ routes.js
@@ -11,19 +11,49 @@ var connection = oracledb.getConnection({
     connectString : "cis450project.ctwsisw9u9yz.us-east-1.rds.amazonaws.com:1521/NBADATA"
 });
 
+function send_query(query, callback) {
+  try {
+    console.log("Attempting connection")
+
+    oracledb.getConnection({
+        user          : 'admin',
+        password      : 'password',
+        connectString : "cis450project.ctwsisw9u9yz.us-east-1.rds.amazonaws.com:1521/NBADATA"
+      }, function(err, connection) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        connection.execute(query, callback);
+
+    });
+  } catch (err) {
+      console.log("Error connecting to database")
+      console.log(err);
+  }
+}
+
 /* ----- ------------------------------- ----- */
 
 
 router.get('/', function(req, res) {
-    var query = 'SELECT *  FROM Draft'; 
-    connection.query(query, function (err, rows, fields) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(rows);
-        res.json(rows);
-      }
-    });  
+  send_query("SELECT * FROM Draft", function(data, err) {
+    if (err) {
+      console.log(err)
+    }
+    else {
+      res.json(data)
+    }
+  });
+    // var query = 'SELECT *  FROM Draft'; 
+    // connection.query(query, function (err, rows, fields) {
+    //   if (err) {
+    //     console.log(err);
+    //   } else {
+    //     console.log(rows);
+    //     res.json(rows);
+    //   }
+    // });  
   });
 
 
